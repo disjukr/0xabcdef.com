71bbb7b21cea5235cffb16b30f455e751dd4c755
JongChan Choi <jong@chan.moe>
2019-01-21 01:22:07 +0900

    Updates

---
+++ pages-manifest-70a353208257f6cf8f47.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{140:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-tsx",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
+//# sourceMappingURL=pages-manifest-70a353208257f6cf8f47.js.map
